6554484a706a0813b7f670e3be7c8e52cbaa7be9
Krisu720 <Krzysiek20131@gmail.com>
2022-08-03 17:11:18 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
 import MuiButton from './components/MuiButton';
 import MuiTypography from './components/MuiTypography';
-
+import MuiTextField from './components/MuiTextField';
+import MuiSelect  from './components/MuiSelect';
+import MuiRadioButton from './components/MuiRadioButton';
+import MuiCheckbox from './components/MuiCheckbox';
+import MuiSwitch from './components/MuiSwitch';
+import MuiRating from './components/MuiRating';
+import MuiAutocomplete from './components/MuiAutocomplete';
 function App() {
   return (
     <div className="App">
         {/* <MuiTypography/> */}
-        <MuiButton/>
+        {/* <MuiButton/> */}
+      {/* <MuiTextField/> */}
+      {/* <MuiSelect/> */}
+      {/* <MuiRadioButton/> */}
+      {/* <MuiCheckbox/> */}
+      {/* <MuiSwitch/> */}
+      {/* <MuiRating/> */}
+      <MuiAutocomplete/>
     </div>
   );
 }
